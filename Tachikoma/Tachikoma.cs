87462dadf97c9178f33d93afe931e04f0ad7f30d
NeilKleistGao <neilkleist@gmail.com>
2026-06-04 21:49:15 +0800

    WIP: Add window config

--- Tachikoma/Tachikoma.cs
+++ Tachikoma/Tachikoma.cs
@@ -1,7 +1,12 @@
 ﻿namespace Tachikoma {
   internal class Tachikoma : Nez.Core {
+    public Tachikoma() : base(isFullScreen: true, windowTitle: "Tachikoma") {
+    }
+
     protected override void Initialize() {
       base.Initialize();
+
+      var handler = Window.Handle;
     }
   }
 }
---
+++ Tachikoma/WindowManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Tachikoma {
+  internal static class WindowManager {
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+    private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool SetLayeredWindowAttributes(IntPtr hWnd, uint crKey, byte bAlpha, uint dwFlags);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern bool UpdateWindow(IntPtr hWnd);
+
+    private const int GWL_EXSTYLE = -20;
+    private const int GWL_STYLE = -16;
+
+    private const uint WS_EX_LAYERED = 0x00080000;
+    private const uint WS_EX_TRANSPARENT = 0x00000020;
+    private const uint WS_EX_TOOLWINDOW = 0x00000080;
+    private const uint WS_CAPTION = 0x00C00000;
+    private const uint WS_THICKFRAME = 0x00040000;
+    private const uint WS_BORDER = 0x00800000;
+
+    private const uint LWA_COLORKEY = 0x01;
+    private const uint LWA_ALPHA = 0x02;
+
+    private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    private const uint SWP_FRAMECHANGED = 0x0020;
+    private const uint SWP_NOMOVE = 0x0002;
+    private const uint SWP_NOSIZE = 0x0001;
+    private const uint SWP_NOACTIVATE = 0x0010;
+    private const uint SWP_SHOWWINDOW = 0x0040;
+    private const uint SWP_NOZORDER = 0x0004;
+  }
+}
